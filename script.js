091fe122be8f4e5696250605393edc82a2e2c8a8
BenW00 <ispyb2000@gmail.com>
2021-10-19 06:16:04 -0400

    Finished exercise excluding bonus

---
+++ script.js
@@ -0,0 +1,70 @@
+
+// Question 1
+function spookyGhost() {
+    console.log("Boo!!!")
+}
+
+spookyGhost()
+
+// Question 2
+function candy(goal) {
+
+    console.log(goal)
+    console.log(goal)
+    console.log(goal)
+    console.log(goal)
+}
+
+candy("Get Candy")
+
+// Question 3
+function werewolf(a, b) {
+    if ( a == "Full" && b == "Moon" )
+        console.log("ARH-WOOO!")
+}
+
+// Question 4
+function halloween() {
+    console.log("October 31st")
+}
+
+// Question 5
+function isEnoughCandy(pieces) {
+    return( number >= 31)
+}
+
+// Question 6
+function candyCounter(nums) {
+    const reducer = (previousValue, currentValue) => previousValue + currentValue;
+    return(nums.reduce(reducer))
+}
+
+// Question 7
+const hauntedMansion = function() {
+    console.log("Welcome, foolish mortals, to the Haunted Mansion!")
+}
+hauntedMansion()
+
+// Question 8
+ghostbusters = () => ("Who You Gonna Call?")
+
+// Question 9
+// No
+
+// Question 10
+// No
+
+// Question 11A
+const costumes = ["Ghost", "Princess", "Pirate"]
+
+// Question 11B
+costumes.forEach(n => {
+    const upperCostumes = (n.toUpperCase());
+});
+
+// BONUS
+
+// Question 12A
+// const stephenKingMovies = ["The Shining", "Christine", "It", "The Mist", "Creepshow", "Pet Sematary] 
+
+// Question 12B
